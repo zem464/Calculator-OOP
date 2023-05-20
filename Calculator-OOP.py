0a1f4eda83ceb5859417928ab81c0a90f9f768b4
Iris Membrere <irismembrere@gmail.com>
2023-05-20 18:27:53 +0800

    Pseudocode for the calculator itself

---
+++ Calculator-OOP.py
@@ -0,0 +1,45 @@
+# Import the class
+
+# Create the object
+
+# Print the intro or greetings
+
+# Ask how many numbers are to be calculated
+
+# Use exception handling
+
+    # For 2 inputs
+
+        # Use while loop
+
+        # Take operation to be used
+
+        # Check input
+
+        # If addition
+
+        # If subtraction
+
+        # If multiplication
+
+        # If division
+
+        # Ask for more calculations
+
+    # For 3 inputs
+
+        # Use while loop
+
+        # Take operation to be used
+
+        # Check input
+
+        # If addition
+
+        # If subtraction
+
+        # If multiplication
+
+        # If division
+
+        # Ask for more calculations
