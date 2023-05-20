75e69ff72a4f370eeaf0dd2d471bd1d7002b5749
Iris Membrere <irismembrere@gmail.com>
2023-05-20 18:41:51 +0800

    Asking for the numbers of inputs

--- Calculator-OOP.py
+++ Calculator-OOP.py
@@ -10,6 +10,8 @@ print("|\033[37;40m                          " + "\033[35;40m\033[1mSIMPLE CALCU
 print("==========================================================================" + "\n")
 
 # Ask how many numbers are to be calculated
+print("\033[33m**Note that this calculator can only except at least 3 inputs.\n")
+num = input("\033[34m\033[1mHow many numbers will you input?: \033[37m\033[0m")
 
 # Use exception handling
 
