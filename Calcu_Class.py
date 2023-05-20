c313549b0f0318a64e7b39bada5e6e7d9484949c
Iris Membrere <irismembrere@gmail.com>
2023-05-20 18:32:05 +0800

    Constructor method for the inputs

--- Calcu_Class.py
+++ Calcu_Class.py
@@ -1,6 +1,9 @@
 # Create class method
-
+class calculator:
     # Create constructor method for the inputs
+    def __init__(self, num1, num2):
+        self.num1 = inp_num1
+        self.num2 = inp_num2
 
     # Create instance method for addition
 
