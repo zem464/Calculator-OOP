557c7e0aeecd6550529b7706b93a7944c348b46c
Iris Membrere <irismembrere@gmail.com>
2023-05-20 18:34:15 +0800

    Instance methods for the operations

--- Calcu_Class.py
+++ Calcu_Class.py
@@ -6,9 +6,17 @@ class calculator:
         self.num2 = inp_num2
 
     # Create instance method for addition
+    def add(self):
+        return num1 + num2
 
     # Create instance method for subtraction
+    def subtract(self):
+        return num1 - num2
 
     # Create instance method for multiplication
+    def multiply(self):
+        return num1 * num2
 
     # Create instance method for division
+    def divide(self):
+        return num1 / num2
