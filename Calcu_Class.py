02656858dac3c9ae8cec8cd40c411815d2e7467e
Iris Membrere <irismembrere@gmail.com>
2023-05-20 18:19:01 +0800

    Pseudocode for the class methods

---
+++ Calcu_Class.py
@@ -0,0 +1,11 @@
+# Create class method
+
+    # Create constructor method for the inputs
+
+    # Create instance method for addition
+
+    # Create instance method for subtraction
+
+    # Create instance method for multiplication
+
+    # Create instance method for division
