bba19342288ac34798eccff4d2164865539ede35
Iris Membrere <irismembrere@gmail.com>
2023-05-20 19:53:31 +0800

    Adding colors

--- Calculator-OOP.py
+++ Calculator-OOP.py
@@ -18,7 +18,7 @@ try:
     # For 2 inputs
     if num == '2':
             # Show operations
-            print("\nIn selecting operations, pick a number.")
+            print("\n\033[33;40m\033[1mIn selecting operations, pick a number.\033[0m")
             print("1. +")
             print("2. -")
             print("3. *")
@@ -27,16 +27,16 @@ try:
             # Use while loop
             while True:
                 # Take operation to be used
-                operations = input("\nOperation: ")
+                operations = input("\n\033[34m\033[1mOperation: \033[37m\033[0m")
 
                 # Check input
                 if operations in ('1', '2', '3', '4'):
                     # Use try-except handling
                     try:
-                        inp_num1 = float(input("Enter number: "))
-                        inp_num2 = float(input("Enter another number: "))
+                        inp_num1 = float(input("\033[34m\033[1mEnter number: \033[37m\033[0m"))
+                        inp_num2 = float(input("\033[34m\033[1mEnter another number: \033[37m\033[0m"))
                     except ValueError:
-                        print("Invalid input. Please enter numbers.")
+                        print("\033[31m\033[1mInvalid input. Please enter numbers.")
                         continue
 
                 # If addition
@@ -56,23 +56,23 @@ try:
                         try:
                             print(inp_num1, "/", inp_num2, "=", calculate.divide(inp_num1, inp_num2))
                         except ZeroDivisionError:
-                            print("Cannot divide by zero")
+                            print("\033[31m\033[1mCannot divide by zero")
                     
                 # Ask for more calculations
-                again = input("More calculations? Put 'n' if none: ")
+                again = input("\n\033[32m\033[1mMore calculations? Put 'n' if none: \033[37m\033[0m")
                     # Continue if there are more calculations
                 if again.lower() == 'n':
-                    print("Thank you!")
+                    print("\n\033[35m\033[1mThank you!")
                     break
             
             # If input is invalid
             else:
-                print("Invalid input.")
+                print("\033[31m\033[1mInvalid input.")
 
     # For 3 inputs
     elif num == '3':
         # Show operations
-        print("\nIn selecting operations, pick a number.")
+        print("\n\033[33;40m\033[1mIn selecting operations, pick a number.\033[0m")
         print("5. +, +")
         print("6. +, -")
         print("7. +, *")
@@ -93,17 +93,17 @@ try:
         # Use while loop
         while True:
             # Take the operations to be used by the users
-            operations = input("\nOperation: ")
+            operations = input("\n\033[34m\033[1mOperation: \033[37m\033[0m")
 
             # Check input
             if operations in ('5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'):
                 # use try-except handling
                 try:
-                    inp_num1 = float(input("Enter number: "))
-                    inp_num2 = float(input("Enter another number: "))
-                    inp_num3 = float(input("Enter another number: "))
+                    inp_num1 = float(input("\033[34m\033[1mEnter number: \033[37m\033[0m"))
+                    inp_num2 = float(input("\033[34m\033[1mEnter another number: \033[37m\033[0m"))
+                    inp_num3 = float(input("\033[34m\033[1mEnter another number: \033[37m\033[0m"))
                 except ValueError:
-                    print("Invalid input. Please enter numbers.")
+                    print("\033[31m\033[1mInvalid input. Please enter numbers.")
                     continue
 
             # If both addition
@@ -123,7 +123,7 @@ try:
                 try:
                     print(inp_num1, "+", inp_num2, "/", inp_num3, "=", calculate.add(inp_num1, inp_num2) / inp_num3)
                 except ZeroDivisionError:
-                    print("Cannot divide by zero")
+                    print("\033[31m\033[1mCannot divide by zero")
             
             # If subtraction and addition
             elif operations == '9':
@@ -142,7 +142,7 @@ try:
                 try:
                     print(inp_num1, "-", inp_num2, "/", inp_num3, "=", calculate.subtract(inp_num1, inp_num2) / inp_num3)
                 except ZeroDivisionError:
-                    print("Cannot divide by zero")
+                    print("\033[31m\033[1mCannot divide by zero")
             
             # If multiplication and addition
             elif operations == '13':
@@ -161,50 +161,50 @@ try:
                 try:
                     print(inp_num1, "*", inp_num2, "/", inp_num3, "=", calculate.multiply(inp_num1, inp_num2) / inp_num3)
                 except ZeroDivisionError:
-                    print("Cannot divide by zero")
+                    print("\033[31m\033[1mCannot divide by zero")
 
             # If division and addition
             elif operations == '17':
                 try:
                     print(inp_num1, "/", inp_num2, "+", inp_num3, "=", calculate.divide(inp_num1, inp_num2) + inp_num3)
                 except ZeroDivisionError:
-                    print("Cannot divide by zero")
+                    print("\033[31m\033[1mCannot divide by zero")
 
             # If division and subtraction
             elif operations == '18':
                 try:
                     print(inp_num1, "/", inp_num2, "-", inp_num3, "=", calculate.divide(inp_num1, inp_num2) - inp_num3)
                 except ZeroDivisionError:
-                    print("Cannot divide by zero")
+                    print("\033[31m\033[1mCannot divide by zero")
 
             # If division and multiplication
             elif operations == '19':
                 try:
                     print(inp_num1, "/", inp_num2, "*", inp_num3, "=", calculate.divide(inp_num1, inp_num2) * inp_num3)
                 except ZeroDivisionError:
-                    print("Cannot divide by zero")
+                    print("\033[31m\033[1mCannot divide by zero")
 
             # If both division
             elif operations == '20':
                 try:
                     print(inp_num1, "/", inp_num2, "/", inp_num3, "=", calculate.divide(inp_num1, inp_num2) / inp_num3)
                 except ZeroDivisionError:
-                    print("Cannot divide by zero")
+                    print("\033[31m\033[1mCannot divide by zero")
 
             # Ask for more calculations
-            again = input("More calculations? Put 'n' if none: ")
+            again = input("\n\033[32m\033[1mMore calculations? Put 'n' if none: \033[37m\033[0m")
                 # Break if no other calculations
             if again.lower() == 'n':
-                print("Thank you!")
+                print("\n\033[35m\033[1mThank you!")
                 break
 
             # If input is invalid
             else:
-                print("Invalid input.")
+                print("\033[31m\033[1mInvalid input.")
 
     # Give choices, if input is not in the choices
     else:
-        print("Please enter 2 or 3 only.")
+        print("\033[31m\033[1mPlease enter 2 or 3 only.")
 
 except ValueError:
-    print("You can only put integers.")
+    print("\033[31m\033[1mYou can only put integers.")
