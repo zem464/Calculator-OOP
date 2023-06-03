565b0ff498572bd7e575ea9bc3c1bd1f7425e91b
Iris Membrere <irismembrere@gmail.com>
2023-06-03 21:02:07 +0800

    Separating the interface for 3-inputs

--- Calculator-OOP.py
+++ Calculator-OOP.py
@@ -1,12 +1,14 @@
 # Import the class
 from User_Interface import UserInterface
 from InputInterface2 import input2
+from InputInterface3 import input3
 from Calcu_Class import calculator
 from Calcu_Class2 import more_ope
 
 # Create the object
 ui = UserInterface()
 ii2 = input2()
+ii3 = input3()
 calculate = calculator()
 calc = more_ope()
 
@@ -71,7 +73,7 @@ if num == '2':
 # For 3 inputs
 elif num == '3':
     # Show operations
-    ui.operations_display_for3()
+    ii3.operations_display_for3()
 
     # Use while loop
     while True:
@@ -88,69 +90,69 @@ elif num == '3':
             # If both addition
             if operations == '5':
                 add_add = calc.add_add(inp_num1, inp_num2, inp_num3)
-                ui.print_add_add(add_add)
+                ii3.print_add_add(add_add)
             
             # If addition and subtraction
             elif operations == '6':
                 add_sub = calc.add_sub(inp_num1, inp_num2, inp_num3)
-                ui.print_add_sub(add_sub)
+                ii3.print_add_sub(add_sub)
 
             # If addition and multiplication
             elif operations == '7':
                 add_multi = calc.add_multi(inp_num1, inp_num2, inp_num3)
-                ui.print_add_multi(add_multi)
+                ii3.print_add_multi(add_multi)
 
             # If addition and division
             elif operations == '8':
                 try:
                     add_div = calc.add_div(inp_num1, inp_num2, inp_num3)
-                    ui.print_add_div(add_div)
+                    ii3.print_add_div(add_div)
                 except ZeroDivisionError:
                     ui.for_ZeroDiv()
             
             # If subtraction and addition
             elif operations == '9':
                 sub_add = calc.sub_add(inp_num1, inp_num2, inp_num3)
-                ui.print_sub_add(sub_add)
+                ii3.print_sub_add(sub_add)
 
             # If both subtraction
             elif operations == '10':
                 sub_sub = calc.sub_sub(inp_num1, inp_num2, inp_num3)
-                ui.print_sub_sub(sub_sub)
+                ii3.print_sub_sub(sub_sub)
                 
             # If subtraction and multiplication
             elif operations == '11':
                 sub_multi = calc.sub_multi(inp_num1, inp_num2, inp_num3)
-                ui.print_sub_multi(sub_multi)
+                ii3.print_sub_multi(sub_multi)
 
             # If subtraction and division
             elif operations == '12':
                 try:
                     sub_div = calc.sub_div(inp_num1, inp_num2, inp_num3)
-                    ui.print_sub_div(sub_div)
+                    ii3.print_sub_div(sub_div)
                 except ZeroDivisionError:
                     ui.for_ZeroDiv()
             
             # If multiplication and addition
             elif operations == '13':
                 multi_add = calc.multi_add(inp_num1, inp_num2, inp_num3)
-                ui.print_multi_add(multi_add)
+                ii3.print_multi_add(multi_add)
 
             # If multiplication and subtraction
             elif operations == '14':
                 multi_sub = calc.multi_sub(inp_num1, inp_num2, inp_num3)
-                ui.print_multi_sub(multi_sub)
+                ii3.print_multi_sub(multi_sub)
 
             # If both multiplication
             elif operations == '15':
                 multi_multi = calc.multi_multi(inp_num1, inp_num2, inp_num3)
-                ui.print_multi_multi(multi_multi)
+                ii3.print_multi_multi(multi_multi)
 
             # If multiplication and division
             elif operations == '16':
                 try:
                     multi_div = calc.multi_div(inp_num1, inp_num2, inp_num3)
-                    ui.print_multi_div(multi_div)
+                    ii3.print_multi_div(multi_div)
                 except ZeroDivisionError:
                     ui.for_ZeroDiv()
 
@@ -158,7 +160,7 @@ elif num == '3':
             elif operations == '17':
                 try:
                     div_add = calc.div_add(inp_num1, inp_num2, inp_num3)
-                    ui.print_div_add(div_add)
+                    ii3.print_div_add(div_add)
                 except ZeroDivisionError:
                     ui.for_ZeroDiv()
 
@@ -166,7 +168,7 @@ elif num == '3':
             elif operations == '18':
                 try:
                     div_sub = calc.div_sub(inp_num1, inp_num2, inp_num3)
-                    ui.print_div_sub(div_sub)
+                    ii3.print_div_sub(div_sub)
                 except ZeroDivisionError:
                     ui.for_ZeroDiv()
 
@@ -174,7 +176,7 @@ elif num == '3':
             elif operations == '19':
                 try:
                     div_multi = calc.div_multi(inp_num1, inp_num2, inp_num3)
-                    ui.print_div_multi(div_multi)
+                    ii3.print_div_multi(div_multi)
                 except ZeroDivisionError:
                     ui.for_ZeroDiv()
 
@@ -182,7 +184,7 @@ elif num == '3':
             elif operations == '20':
                 try:
                     div_div = calc.div_div(inp_num1, inp_num2, inp_num3)
-                    ui.print_div_div(div_div)
+                    ii3.print_div_div(div_div)
                 except ZeroDivisionError:
                     ui.for_ZeroDiv()
 
@@ -194,7 +196,7 @@ elif num == '3':
             
         # If not in the operation choices
         else:
-            ui.not_an_option2()
+            ii3.not_an_option2()
 
     # If input is invalid
     else:
--- User_Interface.py
+++ User_Interface.py
@@ -19,6 +19,7 @@ class UserInterface:
         return operations
     
     def input_num(self):
+        # Ask the user for an input
         try:
             inp_num = float(input("\033[34m\033[1mEnter number: \033[37m\033[0m"))
             return inp_num
@@ -27,76 +28,23 @@ class UserInterface:
             return self.input_num()
     
     def for_ZeroDiv(self):
+        # Displayed on ZeroDivisionError
         print("\033[31m\033[1mCannot divide by zero")
     
     def ask_again(self):
+        # Asking if there are more calculations
         again = input("\n\033[32m\033[1mMore calculations? Put 'n' if none: \033[37m\033[0m")
         if again.lower() == 'n':
             return True
     
     def no_more(self):
+        # Closing greetings if no calculations
         print("\n\033[35m\033[1mThank you!")
     
-    def not_an_option2(self):
-        print("\033[31m\033[1mPick only from 5 to 20")
-    
     def not_operation(self):
+        # Display if the operation is not an integer
         print("\033[31m\033[1mInvalid input.")
-    
-    def operations_display_for3(self):
-        # Show operations
-        print("\n\033[33;40m\033[1mIn selecting operations, pick a number.\033[0m")
-        print("5. +, +\n6. +, -\n7. +, *\n8. +, /\n9. -, +\n10. -, -")
-        print("11. -, *\n12. -, /\n13. *, +\n14. *, -\n15. *, *\n16. *, /")
-        print("17. /, +\n18. /, -\n19. /, *\n20. /, /")
 
     def not_inputsNum(self):
+        # Display if the number for inputs are neither 2 nor 3
         print("\033[31m\033[1mPlease enter 2 or 3 only.")
-
-    def print_add_add(self, add_add):
-        print("=", str(add_add))
-    
-    def print_add_sub(self, add_sub):
-        print("=", str(add_sub))
-    
-    def print_add_multi(self, add_multi):
-        print("=", str(add_multi))
-    
-    def print_add_div(self, add_div):
-        print("=", str(add_div))
-    
-    def print_sub_add(self, sub_add):
-        print("=", str(sub_add))
-    
-    def print_sub_sub(self, sub_sub):
-        print("=", str(sub_sub))
-    
-    def print_sub_multi(self, sub_multi):
-        print("=", str(sub_multi))
-    
-    def print_sub_div(self, sub_div):
-        print("=", str(sub_div))
-
-    def print_multi_add(self, multi_add):
-        print("=", str(multi_add))
-    
-    def print_multi_sub(self, multi_sub):
-        print("=", str(multi_sub))
-    
-    def print_multi_multi(self, multi_multi):
-        print("=", str(multi_multi))
-    
-    def print_multi_div(self, multi_div):
-        print("=", str(multi_div))
-    
-    def print_div_add(self, div_add):
-        print("=", str(div_add))
-    
-    def print_div_sub(self, div_sub):
-        print("=", str(div_sub))
-    
-    def print_div_multi(self, div_multi):
-        print("=", str(div_multi))
-    
-    def print_div_div(self, div_div):
-        print("=", str(div_div))
